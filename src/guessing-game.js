36629f28e81081ae9202cd61186d092ec0faa5d0
KirylDv <39598590+KirylDv@users.noreply.github.com>
2019-10-18 14:08:59 +0300

    add: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let L;
+        let R;
+        let m;
+    }
 
     setRange(min, max) {
-
+        this.L = min;
+        this.R = max;
     }
 
     guess() {
-
+        this.m = Math.ceil((this.L+this.R)/2);
+        return this.m;
     }
 
     lower() {
-
+        this.R = this.m;
     }
 
     greater() {
-
+        this.L = this.m;
     }
 }
 
